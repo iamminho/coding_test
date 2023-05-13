ffa5d1513aa17841ac4df8c83332e1a9e20b8e05
iamminho <minho100227@kakao.com>
2023-05-13 18:58:28 +0900

    수정: 문자열_압축_re

--- src/main/java/codingtest/test/level2/문자열_압축/문자열_압축_re.java
+++ src/main/java/codingtest/test/level2/문자열_압축/문자열_압축_re.java
@@ -1,22 +1,19 @@
 package codingtest.test.level2.문자열_압축;
 
-import java.util.Stack;
-
 public class 문자열_압축_re {
     public int solution(String s) {
         int answer = Integer.MAX_VALUE;
-        if (s.length() == 1) return 1;
 
-        for (int num = 1; num <= s.length() / 2; num++) {
-            answer = Math.min(getPress(num, s), answer);
+        for (int num = 1; num <= s.length(); num++) {
+            answer = Math.min(press(num, s), answer);
         }
 
         return answer;
     }
 
-    private int getPress(int length, String s) {
+    private int press(int length, String s) {
         StringBuilder sb = new StringBuilder();
-        Stack<String> stack = new Stack();
+        String last = "";
         int count = 1;
 
         for (int start = 0; start < s.length(); start += length) {
@@ -24,31 +21,18 @@ public class 문자열_압축_re {
             if (end > s.length()) end = s.length();
             String sub = s.substring(start, end);
 
-            if (stack.isEmpty()) {
-                stack.push(sub);
-                continue;
-            }
-
-            if (stack.peek().equals(sub)) {
-                count++;
-                continue;
-            }
-
-            if (!stack.peek().equals(sub)) {
+            if (!last.equals(sub)) {
                 if (count > 1) {
                     sb.append(count);
                     count = 1;
                 }
-
-                sb.append(stack.pop());
-                stack.push(sub);
-            }
+                sb.append(last);
+                last = sub;
+            } else count++;
         }
 
-        if (!stack.isEmpty()) {
-            if (count > 1) sb.append(count);
-            sb.append(stack.pop());
-        }
+        if (count > 1) sb.append(count);
+        sb.append(last);
 
         return sb.length();
     }
