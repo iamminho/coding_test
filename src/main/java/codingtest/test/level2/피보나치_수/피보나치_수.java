edc5535ba0a80dba582679d2698d2d21343e13bb
iamminho <minho100227@kakao.com>
2023-05-21 08:01:16 +0900

    수정: 피보나치 수

--- src/main/java/codingtest/test/level2/피보나치_수/피보나치_수.java
+++ src/main/java/codingtest/test/level2/피보나치_수/피보나치_수.java
@@ -3,25 +3,22 @@ package codingtest.test.level2.피보나치_수;
 import java.util.Arrays;
 
 public class 피보나치_수 {
-    private static long[] mem;
+    class Solution {
+        private final int[] mem = new int[100001];
 
-    public static void main(String[] args) {
-        int n = 100;
-        mem = new long[n + 1];
-        Arrays.fill(mem, -1);
-        for (int i = 0; i <= n; i++) {
-            fibonacci(n);
+        public int solution(int n) {
+            Arrays.fill(mem, -1);
+            for (int i = 0; i <= n; i++) {
+                fibonacci(n);
+            }
+
+            return fibonacci(n);
         }
 
-        long result = fibonacci(n);
-        System.out.println("result = " + result);
-
-
-    }
-
-    private static long fibonacci(int n) {
-        if (mem[n] != -1) return mem[n];
-        if (n == 0 || n == 1) return n;
-        return mem[n] = fibonacci(n - 1) + fibonacci(n - 2) % 1234567;
+        private int fibonacci(int n) {
+            if (mem[n] != -1) return mem[n];
+            if (n == 0 || n == 1) return n;
+            return mem[n] = (fibonacci(n - 1) + fibonacci(n - 2)) % 1234567;
+        }
     }
 }
